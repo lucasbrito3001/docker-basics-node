ef4de5fc4f0c190812852a411f5383006a495806
Lucas de Brito <devlucasmail@gmail.com>
2022-07-26 12:29:41 -0300

    Learning docker # feat: Install docker and docker-compose and deploy an app with docker in the aws ec2

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const app = require('./src/app.js')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 4200
-const HOST = '0.0.0.0'
 
 app.get('/', (req, res) => res.send('Hello world'))
 
-app.listen(PORT, HOST, () => console.log('> The server is running on port: ' + PORT))
+app.listen(PORT, () => console.log('> The server is running on port: ' + PORT))
